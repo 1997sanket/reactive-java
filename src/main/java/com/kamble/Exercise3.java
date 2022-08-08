f25d8c08d91cc0986223d05fffb18977bcc4839b
Sanket Kamble <1997sanket@gmail.com>
2022-08-08 15:12:14 +0530

    Done till Exercise 5 handling completion and error events.

--- src/main/java/com/kamble/Exercise3.java
+++ src/main/java/com/kamble/Exercise3.java
@@ -1,6 +1,8 @@
 package com.kamble;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise3 {
 
@@ -10,11 +12,10 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        List<Integer> list = ReactiveSources.intNumbersFlux().toStream().collect(Collectors.toList());
 
+        System.out.println("List: " + list);
 
-        System.out.println("Press a key to end");
-        System.in.read();
     }
 
 }
