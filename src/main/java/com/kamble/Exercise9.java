aeeefc19eaf136ba968b243a13e01e5e4b9f18c4
Sanket Kamble <1997sanket@gmail.com>
2022-09-16 12:29:00 +0530

    added exercise 9

--- src/main/java/com/kamble/Exercise9.java
+++ src/main/java/com/kamble/Exercise9.java
@@ -10,13 +10,16 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        //ReactiveSources.intNumbersFlux().count().subscribe(System.out::println);  //counts while emitting
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        //ReactiveSources.intNumbersFlux().collectList().subscribe(System.out::println);  //prints list when last element is emitted
 
-        // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        // Transform to a sequence of sum of adjacent two numbers
+        ReactiveSources.intNumbersFlux()
+                .buffer(2)  //buffer of 2 means, 2 events = 1 event
+                .map(list -> list.get(0) + list.get(1))
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
